84ef6d0f4afa1cb7b5b155f61b7ca46525024f52
Asajj Ventress <hermanadelanoche@gmail.com>
2020-11-09 00:18:41 +0100

    Update and rename README.pm to README

--- README.pm
+++
@@ -1,3 +0,0 @@
-Este directorio contiene mis programas
-  troceafichero.py => trocea el fichero rockyou.txt en ficheros de 20000 lineas. Normalmente los ficheros de 
-  wordlists son muy tochos por lo que el ataque de fuerza bruta se hace eterno y terminan echándote.
